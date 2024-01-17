b4c6afccdfcdb03b1d3204c3aefcdd3c8fb5c410
codegabb <gab.blenden@gmail.com>
2024-01-16 20:41:34 -0600

    Studio class 2

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -18,18 +18,35 @@ let weatherStatus = "clear";
 let preparedForLiftOff = true;
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
-if (astronautCount <= 7 && astronautStatus === "ready" && totalMassKg < maximumMassLimit &&
-    (fuelTempCelsius >= -300 && fuelTempCelsius <= -150) && fuelLevel === "100%" &&
-    weatherStatus === "clear");
+if (astronautCount <= 7)
+    console.log(preparedForLiftOff)
 
 // add logic below to verify all astronauts are ready
+if (astronautStatus === "ready")
+    console.log(preparedForLiftOff)
 
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
+if (totalMassKg < 850000)
+    console.log(preparedForLiftOff)
 
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
+if ((fuelTempCelsius > -300) && (fuelTempCelsius < -150)) 
+    console.log(preparedForLiftOff)
+
 
 // add logic below to verify the fuel level is at 100%
+if (fuelLevel = "100%")
+    console.log(preparedForLiftOff)
+
 
 // add logic below to verify the weather status is clear
 
+if (weatherStatus = "clear")
+    console.log(preparedForLiftOff)
+
+
 // Verify shuttle launch can proceed based on above conditions
+
+if (preparedForLiftOff) {
+        console.log("Have a safe flight astronauts!");
+      }
